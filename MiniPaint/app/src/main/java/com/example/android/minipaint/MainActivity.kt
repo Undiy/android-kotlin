d411f37e0280aafdb40f889b61b86f4951b35278
Undiy <undiy@yandex.ru>
2023-11-17 14:13:25 +0500

    MiniPaint: better fullscreen mode

--- MiniPaint/app/src/main/java/com/example/android/minipaint/MainActivity.kt
+++ MiniPaint/app/src/main/java/com/example/android/minipaint/MainActivity.kt
@@ -1,17 +1,48 @@
 package com.example.android.minipaint
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
-import android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myCanvasView: MyCanvasView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val myCanvasView = MyCanvasView(this)
-        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_HIDE_NAVIGATION or SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView = MyCanvasView(this)
         myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
         setContentView(myCanvasView)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUI()
+        } else {
+            showSystemUI()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        showSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, myCanvasView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    private fun showSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        WindowInsetsControllerCompat(window, myCanvasView).show(WindowInsetsCompat.Type.systemBars())
     }
 }
